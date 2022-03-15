2e93b35bfacafa5c7d039cc1db3191f2b111b9a0
Lincoln Yan <yyjlincoln@gmail.com>
2022-03-15 17:23:50 +1100

    Added a simple calculator

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function sayHello() {
 function sayGoodbye() {
     alert("Goodbye :(");
 }
+function updateSum() {
+    const numberOneInput = document.getElementById("number-1-input");
+    const numberTwoInput = document.getElementById("number-2-input");
+    const numberOne = Number(numberOneInput.value);
+    const numberTwo = Number(numberTwoInput.value);
+    const sum = numberOne + numberTwo;
+    const sumElement = document.getElementById("sum-output");
+    sumElement.innerText = sum;
+}
