7cd12a7f92849c803cd6444df72746c61753befe
Tong <tongzhou33@hotmail.com>
2022-05-10 16:53:32 +1000

    asd

--- index.js
+++ index.js
@@ -45,9 +45,19 @@ function movingblock() {
     clearInterval(id);
     id = setInterval(frame, 5);
     function frame() {
-    if (pos == 360) {
-      clearInterval(id);
-    } else {
+    if (pos > 1000) {
+        clearInterval(Id);
+    }
+    else if (pos >= 720) {
+      pos++;
+      elem.style.top = pos - "px"; 
+      elem.style.left = pos - "px"; 
+    }
+    else if (pos >= 350) {
+        pos++;
+        elem.style.top = pos + "px"; 
+    } 
+    else {
       pos++; 
       elem.style.top = pos + "px"; 
       elem.style.left = pos + "px"; 
